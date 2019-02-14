3bef09cf22c73995b70804d7643cd6ad98f8f5cf
Manmohan Kumar <csemanmohan@gmail.com>
2019-02-14 15:36:56 +0300

    updated with flask..

--- server.py
+++ server.py
@@ -1,15 +1,15 @@
-from aiohttp import web
+from flask import Flask
+app = Flask(__name__)
 
 
-async def handle(request):
-    name = request.match_info.get('name', "World!")
-    text = "Hello, " + name
-    print('received request, replying with "{}".'.format(text))
-    return web.Response(text=text)
+@app.route('/', methods=['GET'])
+def reply():
+    """
+    method for getting file downloaded from s3
+    :return: reply
+    """
+    res = "Hello World"
+    return res
 
 
-app = web.Application()
-app.router.add_get('/', handle)
-app.router.add_get('/{name}', handle)
-
-web.run_app(app, port=5858)
+app.run(debug=True, port=5858)
